42603c3b6aa8f84d8d0ee1efb365f490ed965198
madisondegrezia <89614960+madisondegrezia@users.noreply.github.com>
2023-02-15 00:44:03 -0500

    minor fix

--- src/components/ArrayMap.jsx
+++ src/components/ArrayMap.jsx
@@ -3,7 +3,7 @@ import products from "../utils/products";
 const ArrayMap = () => {
 
     return <ul>
-        {products.map((product, index, id) => {
+        {products.map((product, index) => {
             return <li className="text-center" key={index}>{index+1}. {product.name}</li>
         })}
     </ul>;
